6b8e68cba78de656cc1707c219032d7177fe27c0
RiddhiDamani <riddhidamani90@gmail.com>
2021-03-11 01:17:06 -0600

    V1.0 Updated tabs section of JS file

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@
     tabsContainer = document.querySelector(".about-tabs");
 
     tabsContainer.addEventListener("click", (event) => {
-        console.log(event.target);
+        // if event.target contains 'tab-item' class, but not 'active' class
+        if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")) {
+            const target = event.target.getAttribute("data-target");
+            // deactivate existing active 'tab-item'
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+            // activate new 'tab-item'
+            event.target.classList.add("active", "outer-shadow");
+            // deactivate exisiting active 'tab-content'
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
+            // activate new 'tab-content'
+            aboutSection.querySelector(target).classList.add("active");
+        }
     })
 })();
 
