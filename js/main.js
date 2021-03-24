57be8905aa7bbedad08f32eb36f761df1e145c92
RiddhiDamani <riddhidamani90@gmail.com>
2021-03-24 13:37:22 -0500

    V1.0 Updated Portfolio Section - Categorized Latest Works

--- js/main.js
+++ js/main.js
@@ -19,3 +19,43 @@
     })
 })();
 
+/* ------------------ Portfolio Filter and Pop-Up -----------------  */
+
+(() => {
+    const filterContainer = document.querySelector(".portfolio-filter"),
+     portfolioItemsContainer = document.querySelector(".portfolio-items"),
+     portfolioItems = document.querySelectorAll(".portfolio-item"), 
+     popup = document.querySelector(".portfolio-popup"),
+     prevBtn = popup.querySelector(".pp-prev"),
+     nextBtn = popup.querySelector(".pp-next"),   
+     closeBtn = popup.querySelector(".pp-close"),
+     projectDetailsContainer = popup.querySelector(".pp-details"),
+     projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+
+    let itemIndex, slideIndex, screenshots;
+
+    /* ------------------ Filter Portfolio Items -----------------  */
+    filterContainer.addEventListener("click", (event) => {
+        if(event.target.classList.contains("filter-item") && (!event.target.classList.contains("active"))) {
+            //Deactivate existing active 'filter-item'
+            filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+            // Activate new 'filter Item'
+            event.target.classList.add("active","outer-shadow");
+            const target = event.target.getAttribute("data-target");
+            // console.log(target);
+            portfolioItems.forEach((item) => {
+                // console.log(item);
+                // console.log(item.getAttribute("data-category"));
+                if(target === item.getAttribute("data-category") || target == 'all') {
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                }
+                else {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            }) 
+        }
+        
+    })
+})();
