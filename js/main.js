8e68a2b269badf4800784b20e30b30ec6c84a286
RiddhiDamani <riddhidamani90@gmail.com>
2021-03-25 14:53:51 -0500

    V1.0 Updated Portfolio Section - Testimonial Section Added

--- js/main.js
+++ js/main.js
@@ -183,3 +183,55 @@ function bodyScrollingToggle () {
         }
     }
 })();
+
+/* ------------------ Testimonial Slider -----------------  */
+
+(() => {
+    const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider-nav .next"),
+    activeSlide = sliderContainer.querySelector(".testi-item.active");
+    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+    //console.log(slideWidth);
+    //console.log(slideIndex);
+    // Set width of all slides
+    slides.forEach((slide) => {
+        //console.log(slide);
+        slide.style.width = slideWidth + "px";
+    }) 
+    // Set width of the slider container
+    sliderContainer.style.width = slideWidth * slides.length + "px";
+
+    nextBtn.addEventListener("click", () => {
+        if(slideIndex === slides.length - 1) {
+            slideIndex = 0;
+        }
+        else {
+            slideIndex++;
+        }
+        //console.log(slideIndex);
+        slider();
+    })
+
+    prevBtn.addEventListener("click", () => {
+        if(slideIndex === 0) {
+            slideIndex = slides.length - 1;
+        }
+        else {
+            slideIndex--;
+        }
+        //console.log(slideIndex);
+        slider();
+    })
+
+    function slider() {
+        // Deactivate existing active slides
+        sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+        // Activate new slide
+        slides[slideIndex].classList.add("active");
+        sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+    }
+    slider();
+})();
