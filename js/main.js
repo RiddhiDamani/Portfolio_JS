aba9ad3db1e8ae1390edd0b538dac419135b1f99
RiddhiDamani <riddhidamani90@gmail.com>
2021-03-27 21:44:02 -0500

    V1.0 Updated Contact Form Submission Code

--- js/main.js
+++ js/main.js
@@ -334,27 +334,55 @@ window.addEventListener("load", () => {
     }, 600)
 })
 
-/* ------------------ Contact Form Submission -----------------  */
+// /* ------------------ Contact Form Submission -----------------  */
 
-var form = document.getElementById("my-form");
-    
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("outer-shadow");
+      status.classList.add("hover-in-shadow");
+      status.classList.add("success");
+      status.innerHTML = "Thank You! Your message has been sent successfully.";
     }
-  }).then(response => {
-    status.classList.add("Success");
-    status.innerHTML = "Thanks for your submission!";
-    form.reset()
-  }).catch(error => {
-    status.classList.add("Error");
-    status.innerHTML = "Oops! There was a problem submitting your form"
+  
+    function error() {
+      status.classList.add("outer-shadow");
+      status.classList.add("hover-in-shadow");
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
   });
-}
-form.addEventListener("submit", handleSubmit)
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
